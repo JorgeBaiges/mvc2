429aa0b69ececc2979e17fe3c6a4c776227f7e2d
Jorge Baiges <joosbaiges@gmail.com>
2022-11-23 17:20:10 +0000

    mvc06 okay

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
     namespace App\Controllers;
+    use Dompdf\Dompdf;
     include "../Product.php";
 
     class ProductController{
@@ -21,5 +22,15 @@
             $product = \Product::find($id);
             require "../views/show.php";
             // metodo show de Controller de mvc00
+        }
+
+        public function pdf(){
+            include_once "../vendor/autoload.php";
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml('<h1>Hola mundo</h1><br><a href="https://parzibyte.me/blog">By Parzibyte</a>');
+            $dompdf->render();
+            $contenido = $dompdf->output();
+            $nombreDelDocumento = "hola.pdf";
+            $bytes = file_put_contents($nombreDelDocumento, $contenido);
         }
     }//Fin_Clase
