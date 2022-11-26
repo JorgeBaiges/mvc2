431a29c2295c7f952696514345d9b8073f653ecf
JorgeBaiges <91206307+JorgeBaiges@users.noreply.github.com>
2022-11-26 19:53:06 +0100

    fora

--- start.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    //Recurso/metodo/parametro
-        //recurso : controladores
-        // accion : metodos del controlador -> show() , find
-        // parametros  :  find -> id de producto.
-        require_once "core/App.php";
-        $app = new \Core\App();
